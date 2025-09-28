cfa7d671330065a42ef86098476179593b3469ff
GuiTavs7 <7tavs7@gmail.com>
2025-09-28 13:32:30 -0300

    first commit

---
+++ src/components/StatusBar/StatusBar.jsx
@@ -0,0 +1,12 @@
+import { useOnlineStatus } from '../../hooks/useOnlineStatus';
+
+export default function StatusBar() {
+
+    const isOnline = useOnlineStatus();
+
+    return(
+        <>
+            <h1>{isOnline ? "✔ Online" : "✖ Offline"}</h1>
+        </>
+    )
+}
